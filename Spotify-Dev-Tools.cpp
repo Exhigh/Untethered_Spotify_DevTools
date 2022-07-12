f3189f9ed2a1d9ea98770db3754f24d52dee5070
ExHigh <exhigh01@gmail.com>
2022-07-12 07:43:54 +0400

    Refactor Code Base ^_^
    * Refactor the code to be compiled with g++.
    * Split the functions to a separate header file.
    * Remove unnecessary goto statements and refactored their use case.
    * Moved the map container to an unordered map container.
    * Improve code paths that might have been mishandled before (thread handling I am looking at you).
    * Replaced old and deprecated functions with their newer implementations.
    * Removed some unused header files.
    
    Result:
    * Search time for the memory address to be patched is significantly faster.
    * Final executable size is 83.95% smaller than the previous implementation's executable.
    * Cleaner implementation (Maybe ?)

--- Spotify-Dev-Tools.cpp
+++ Spotify-Dev-Tools.cpp
@@ -1,141 +1,42 @@
-/*	
-  @initial author: ririxidev <dev@ririxi.dev>
-  @author: Exhigh <exhigh01@gmail.com> 
-*/
-#include <windows.h>
-#include <psapi.h>
-#include <iostream>
-#include <conio.h>
-#include <tlhelp32.h>
-#include <vector.h>
-#include <string.h>
-#include <tchar.h>
-#include <map.h>
+//@author: Exhigh <exhigh01@gmail.com>
 
-#pragma comment(lib, "psapi.lib")
-
-int SpotifyVersionMap(std::string x)
-{
-	std::map<std::string, int> version;
-	
-	version.insert(std::pair<std::string, int>("1.1.80.699", 0x16525FD));
-	version.insert(std::pair<std::string, int>("1.1.81.598", 0x170273E));
-	version.insert(std::pair<std::string, int>("1.1.81.604", 0x170B73E));
-	version.insert(std::pair<std::string, int>("1.1.82.754", 0x16E5A7E));
-	version.insert(std::pair<std::string, int>("1.1.82.758", 0x16E4A7E));
-	version.insert(std::pair<std::string, int>("1.1.82.760", 0x170BAFE));
-	version.insert(std::pair<std::string, int>("1.1.83.944", 0x16AAC3D));
-	version.insert(std::pair<std::string, int>("1.1.83.954", 0x16AAC3D));
-	version.insert(std::pair<std::string, int>("1.1.83.956", 0x16ACC3D));
-	version.insert(std::pair<std::string, int>("1.1.84.716", 0x16A597C));
-	version.insert(std::pair<std::string, int>("1.1.85.884", 0x16ACCBC));
-	version.insert(std::pair<std::string, int>("1.1.85.895", 0x16B7C7C));
-	version.insert(std::pair<std::string, int>("1.1.86.857", 0x16EFC3D));
-	version.insert(std::pair<std::string, int>("1.1.87.612", 0x1700ABD));
-	version.insert(std::pair<std::string, int>("1.1.88.595", 0x17121C5));
-	version.insert(std::pair<std::string, int>("1.1.88.612", 0x17131C5));
-	version.insert(std::pair<std::string, int>("1.1.89.854", 0x16F9C86));
-	version.insert(std::pair<std::string, int>("1.1.89.858", 0x16FBC86));
-	version.insert(std::pair<std::string, int>("1.1.89.862", 0x16FCC86));
-	
-	std::map<std::string, int>::iterator xe = version.find(x);
-	return ((xe != version.end()) ? xe->second : 0);
-}
-
-HANDLE GetProcessInfo(char* processName, char **processPath)
-{
-	HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-	PROCESSENTRY32 entry;
-	entry.dwSize = sizeof(entry);
-	char exePath[MAX_PATH];
-
-	do {
-		if (!strcmp(entry.szExeFile, processName)) {
-			DWORD targetID = entry.th32ProcessID;
-			CloseHandle(handle);
-			HANDLE TargetProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_ALL_ACCESS, false, targetID);
-			if (TargetProcess != NULL) {
-				if (GetModuleFileNameEx(TargetProcess, NULL, exePath, MAX_PATH) != 0) {
-					*processPath = strdup(exePath);
-					return TargetProcess;
-				} else {
-					CloseHandle(TargetProcess);
-				}
-			}
-		}
-	}
-	while (Process32Next(handle, &entry));
-
-	return 0;
-}
-
-char* GetProcessVersion (char* processPath)
-{
-	DWORD dwHandle, dwLen;
-	UINT BufLen;
-	LPTSTR lpData, lpBuffer = NULL, LibName;
-	VS_FIXEDFILEINFO* pFileInfo;
-	
-	if (processPath == NULL) {
-		// Beep Boop Beep, Wrong Turn, Return To Safety Before Armageddon
-		return NULL;
-	}
-	LibName = processPath;
-	dwLen = GetFileVersionInfoSize (LibName, &dwHandle);
-	if (dwLen) {
-		lpData = (LPTSTR) malloc (dwLen);
-		if (lpData) {
-			if (GetFileVersionInfo (LibName, dwHandle, dwLen, lpData)) {
-				if (VerQueryValue (lpData, "\\", (LPVOID*) &pFileInfo, (PUINT) &BufLen)) {
-					lpBuffer = (LPTSTR) malloc(sizeof(pFileInfo));
-					sprintf(lpBuffer,"%d.%d.%d.%d", HIWORD(pFileInfo->dwFileVersionMS),LOWORD(pFileInfo->dwFileVersionMS),HIWORD(pFileInfo->dwFileVersionLS),LOWORD(pFileInfo->dwFileVersionLS));
-				}
-			}
-		}
-	}
-	free (lpData);
-	return lpBuffer;
-}
+#include "Spotify-dev-tools.h"
 
 int main()
 {
-
 	HANDLE process;
 	std::string dummy, spotifyVersion;
-	int address, fail = 0;
-	char *processVersion, *spotifyPath = NULL;
-	Retry:
-	std::cout<<"[!] This program works only for Spotify ^_^ "<<std::endl;
-	process = GetProcessInfo("Spotify.exe",&spotifyPath);
-	if(!process) {
-		fail++;
-		std::cerr<<"[-] Can't hook up to process (Is Spotify Open ?)"<<std::endl;
-		std::cout<<"[!] Open Spotify And Hit Any Key To Continue"<<std::endl;
-		std::cout<<"\n[!] Number of Times, Dev Tools Failed To Be Activated: "<<fail<<std::endl;
-		std::getline(std::cin, dummy);
-		clrscr();
-		goto Retry;
-	}
-	processVersion = GetProcessVersion(*&spotifyPath);
-	if (processVersion == NULL) {
-		// Clearly This Wouldn't Have Ended Well, Let's Retry
-		clrscr();
-		goto Retry;
-	}
-	spotifyVersion = (char*) malloc(sizeof(processVersion) + 1); 
+	uintptr_t address;
+	int fail = 0;
+	char* processVersion, *spotifyPath = NULL;
+	do {
+		std::cout << "[!] This program works only for Spotify ^_^ " << std::endl;
+		process = GetProcessInfo("Spotify.exe", &spotifyPath);
+		processVersion = GetProcessVersion(*&spotifyPath);
+		if (!process || (processVersion == NULL)) {
+			fail++;
+			std::cerr << "[-] Can't hook up to process (Is Spotify Open ?)" << std::endl;
+			std::cout << "[!] Open Spotify And Hit Any Key To Continue" << std::endl << std::endl;
+			std::cout << "[!] Number of Times, Dev Tools Failed To Be Activated: " << fail << std::endl;
+			std::getline(std::cin, dummy);
+			clear_screen();
+		}
+	} while (!process);
+
+	spotifyVersion = (char*)malloc(sizeof(processVersion) + 1);
 	spotifyVersion = processVersion;
-	std::cout<<"Current Spotify Version - "<<spotifyVersion<<std::endl;
+	std::cout << "Current Spotify Version - " << spotifyVersion << std::endl;
 
 	address = SpotifyVersionMap(spotifyVersion);
 
 	int targetValue = 255;
 	int result = WriteProcessMemory(process, (LPVOID*)address, &targetValue, (DWORD)sizeof(targetValue), NULL);
 	if (result == 0x00 || address == 0) {
-		std::cerr<<"[-] There was some error, try again later or open an issue, with the Spotify Version!"<<std::endl;
+		std::cerr << "[-] There was some error, try again later or open an issue, with the Spotify Version!" << std::endl;
 	} else {
-		std::cout<<"[+] Developer mode has been successfully enabled."<<std::endl;
+		std::cout << "[+] Developer mode has been successfully enabled." << std::endl;
 	}
-	std::cout<<"[!] Press Any Key To Exit"<<std::endl;
+	std::cout << "[!] Press Any Key To Exit" << std::endl;
 	std::getline(std::cin, dummy);
 	return 0;
 }
---
+++ Spotify-Dev-Tools.h
@@ -0,0 +1,108 @@
+#include <windows.h>
+#include <psapi.h>
+#undef UNICODE
+#include <tlhelp32.h>
+#define UNICODE
+#include <iostream>
+#include <tchar.h>
+#include <string>
+#include <unordered_map>
+
+#pragma comment(lib, "version.lib")
+
+uintptr_t SpotifyVersionMap(std::string x)
+{
+	std::unordered_map<std::string, int> version;
+
+	version.insert(std::pair<std::string, int>("1.1.80.699", 0x16525FD));
+	version.insert(std::pair<std::string, int>("1.1.81.598", 0x170273E));
+	version.insert(std::pair<std::string, int>("1.1.81.604", 0x170B73E));
+	version.insert(std::pair<std::string, int>("1.1.82.754", 0x16E5A7E));
+	version.insert(std::pair<std::string, int>("1.1.82.758", 0x16E4A7E));
+	version.insert(std::pair<std::string, int>("1.1.82.760", 0x170BAFE));
+	version.insert(std::pair<std::string, int>("1.1.83.944", 0x16AAC3D));
+	version.insert(std::pair<std::string, int>("1.1.83.954", 0x16AAC3D));
+	version.insert(std::pair<std::string, int>("1.1.83.956", 0x16ACC3D));
+	version.insert(std::pair<std::string, int>("1.1.84.716", 0x16A597C));
+	version.insert(std::pair<std::string, int>("1.1.85.884", 0x16ACCBC));
+	version.insert(std::pair<std::string, int>("1.1.85.895", 0x16B7C7C));
+	version.insert(std::pair<std::string, int>("1.1.86.857", 0x16EFC3D));
+	version.insert(std::pair<std::string, int>("1.1.87.612", 0x1700ABD));
+	version.insert(std::pair<std::string, int>("1.1.88.595", 0x17121C5));
+	version.insert(std::pair<std::string, int>("1.1.88.612", 0x17131C5));
+	version.insert(std::pair<std::string, int>("1.1.89.854", 0x16F9C86));
+	version.insert(std::pair<std::string, int>("1.1.89.858", 0x16FBC86));
+	version.insert(std::pair<std::string, int>("1.1.89.862", 0x16FCC86));
+
+	std::unordered_map<std::string, int>::iterator xe = version.find(x);
+	return ((xe != version.end()) ? xe->second : 0);
+}
+
+void clear_screen()
+{
+	// code excerpt from:
+	// https://social.msdn.microsoft.com/Forums/en-US/1e493962-663e-404c-b951-5e9507cbb279/problems-with-switch?forum=Vsexpressvc
+
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coord = { 0, 0 };
+	DWORD count;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hStdOut, &csbi);
+	FillConsoleOutputCharacterA(hStdOut, ' ', csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+	SetConsoleCursorPosition(hStdOut, coord);
+}
+
+HANDLE GetProcessInfo(const char* processName, char** processPath)
+{
+	HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(entry);
+	char exePath[MAX_PATH];
+
+	do {
+		if (!strcmp(entry.szExeFile, processName)) {
+			DWORD targetID = entry.th32ProcessID;
+			HANDLE targetProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_ALL_ACCESS, false, targetID);
+			if (targetProcess != NULL) {
+				if (GetModuleFileNameExA(targetProcess, NULL, exePath, MAX_PATH) != 0) {
+					*processPath = _strdup(exePath);
+					CloseHandle(handle);
+					return targetProcess;
+				} else {
+					CloseHandle(targetProcess);
+				}
+			}
+		}
+	} while (Process32Next(handle, &entry));
+
+	CloseHandle(handle);
+	return 0;
+}
+
+char* GetProcessVersion(char* processPath)
+{
+	DWORD dwHandle, dwLen;
+	UINT bufLen;
+	LPSTR lpData = NULL, lpBuffer = NULL, libName;
+	VS_FIXEDFILEINFO* pFileInfo;
+
+	if (processPath == NULL) {
+		// Beep Boop Beep, Wrong Turn, Return To Safety Before Armageddon
+		return NULL;
+	}
+	libName = processPath;
+	dwLen = GetFileVersionInfoSizeA(libName, &dwHandle);
+	if (dwLen) {
+		lpData = (LPSTR)malloc(dwLen);
+		if (lpData) {
+			if (GetFileVersionInfoA(libName, dwHandle, dwLen, lpData)) {
+				if (VerQueryValueA(lpData, "\\", (LPVOID*)&pFileInfo, (PUINT)&bufLen)) {
+					lpBuffer = (LPSTR)malloc(3 * sizeof(DWORD));
+					sprintf_s(lpBuffer, (3 * sizeof(DWORD)), "%d.%d.%d.%d", HIWORD(pFileInfo->dwFileVersionMS), LOWORD(pFileInfo->dwFileVersionMS), HIWORD(pFileInfo->dwFileVersionLS), LOWORD(pFileInfo->dwFileVersionLS));
+				}
+			}
+		}
+	}
+	free(lpData);
+	return lpBuffer;
+}
