6477f5b954107f76aff6bb01b745002f76524433
ExHigh <exhigh01@gmail.com>
2022-08-02 19:58:08 +0400

    Update Version Compatibility

--- Spotify-Dev-Tools.h
+++ Spotify-Dev-Tools.h
@@ -35,6 +35,7 @@ uintptr_t SpotifyVersionMap(std::string x)
 	version.insert(std::pair<std::string, int>("1.1.89.862", 0x16FCC86));
 	version.insert(std::pair<std::string, int>("1.1.90.855", 0x16F1C7E));
 	version.insert(std::pair<std::string, int>("1.1.90.859", 0x1700C86));
+	version.insert(std::pair<std::string, int>("1.1.91.824", 0x16E0805));
 
 	std::unordered_map<std::string, int>::iterator xe = version.find(x);
 	return ((xe != version.end()) ? xe->second : 0);
